3a621eb975350293ff5ac06b7f1c939956dbe78d
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-31 12:51:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,3 +18,10 @@ function forLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5
   }
+  function doWhileLoop(array){
+     do {
+       array.pop();
+     } while (array.length > 0 && maybeTrue());
+   
+     return array;
+   }
