f3423f16d7962f30ec4f48fa5141a876be3fe413
darraghmcalone@hotmail.co.uk <darraghmcalone@hotmail.co.uk>
2017-10-31 12:38:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,9 @@
-for (var i = 1; i < 25; i++) {
-  console.log( "I am " + i + "strange" );
+function forLoop(array)
+for (let i = 1; i < 26; i++) {
+  if (i==1){
+  array.push( "I am " + i + "strange" );
+  } else {
+  array.push(`I am ${i strange loops.`);
+  }
 }
+return array
